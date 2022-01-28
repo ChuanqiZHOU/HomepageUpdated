0d3ae938c6e2161a55af873cec083bbfa05c921f
ChuanqiZHOU <chuanqizhou@gmail.com>
2022-01-29 00:05:32 +1300

    commit to master

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { Routes, Route } from 'react-router-dom'
 
+// import rendering pages
+import Home from './pages/Home';
 
 
 function App() {
   return (
-    <div className="App">
-      This is from Bill ZHOU
+    <div className="App">    
+    <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+// App page is used to build the Route rules
---
+++ src/pages/Home/index.js
@@ -0,0 +1,9 @@
+import styles from './index.css'
+
+const Home = () => {
+    return (
+       <div> this is from Home component</div>
+    )
+}
+
+export default Home
